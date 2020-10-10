f9a6be762192d465d69f88ea169b30442cc72017
KarthikPurna <karthik.purna514@gmail.com>
2020-10-10 00:14:03 -0700

    chanes done in mangag.py for checking purpose

--- voyagers/manage.py
+++ voyagers/manage.py
@@ -13,7 +13,8 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual environment? \
+            please make sure  to add Django."
         ) from exc
     execute_from_command_line(sys.argv)
 
