fea5f8ec9627ecf3fb229f1de6cc1ad907c0c558
trnguyen1510 <trnguyen1510@gmail.com>
2020-10-12 15:04:23 -0700

    added frontend but not done

--- voyagers/home/urls.py
+++ voyagers/home/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>', views.index, name='detail'),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/',views.about, name='about-us'),
+    path('login/', views.login, name='login')
 
 
 ]
--- voyagers/home/views.py
+++ voyagers/home/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 # Create your views here.
 def index(request):
 # Render the HTML template index.html
     return render(request, 'index.html')
+
+def about(request):
+    return render(request,'about-us.html')
+
+def login(request):
+    return render(request,'login.html')    
