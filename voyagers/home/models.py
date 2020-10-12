870ea4f4a5f34bf8f2af828ad0fa90e6bf66062c
Jerry Jiang <yangeng.jiang@cgu.edu>
2020-10-11 21:23:37 -0700

    userprofile

--- voyagers/home/models.py
+++ voyagers/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class User_Profile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    Address = models.TextField()
+    Country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
