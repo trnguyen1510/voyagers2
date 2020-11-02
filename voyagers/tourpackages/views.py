7d234970870a781336d36b47361ef9ac39cf00ed
trnguyen1510 <trnguyen1510@gmail.com>
2020-11-01 18:11:14 -0800

    search bar is done

--- voyagers/tourpackages/views.py
+++ voyagers/tourpackages/views.py
@@ -6,16 +6,36 @@ from django.db.models import Q
 
 # Create your views here.
 def tours(request):
+ 
     attractions = Attraction.objects.all()
     context = dict(attraction=attractions)
+    if(request.GET.get('q')):
+        a = (request.GET.get('q'))
+        attractionSearch = Attraction.objects.all().filter(
+            city__icontains=a)
+        context = dict(attraction=attractionSearch)
+
+    # attractionSearch = Attraction.objects.all().filter(attractionName='Shanghai')
     return render(request, 'tours.html', context)
 
-# class tours(ListView):
-#     model = Attraction
-#     template_name = 'tours.html'
 
-#     def get_queryset(self):
-#         query = self.request.GET.get('q')
-#         attraction = Attraction.objects.filter(
-#             Q(city__icontains=query))
-#         return attraction
+# class attractionSearch(ListView):
+    # paginate_by = 10
+
+# def get_queryset(request,self):
+#     attractionSearch = Attraction.objects.all().filter(attractionName='Shanghai')
+#     contexts= dict(attraction=attractionSearch)
+   
+#     # result = super(ListView, self).get_queryset()
+
+#     # query = self.request.GET.get('q')
+#     # if query:
+#     #     query_list = query.split()
+#     #     result = result.filter(
+#     #         reduce(operator.and_,
+#     #                (Q(attractionName__icontains=q) for q in query_list))
+#     #     )
+
+#     return render(request, 'tours.html', contexts)
+
+
