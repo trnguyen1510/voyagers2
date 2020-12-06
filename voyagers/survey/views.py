852230fd6f197bdfe30507bfa4c210bc0ecaa376
trnguyen1510 <trnguyen1510@gmail.com>
2020-12-05 22:19:04 -0800

    updating the front end for tourdetail

--- voyagers/survey/views.py
+++ voyagers/survey/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
 from .models import Survey
-# from .forms import *
-# from django.forms.models import inlineformset_factory
-# from django.core.exceptions import PermissionDenied
 import json
 from django.http import HttpResponse
 from dashboard.forms import UserForm
 from django.forms.models import inlineformset_factory
 from django.core.exceptions import PermissionDenied
+from .forms import surveyForm
 
 # Create your views here.
 
@@ -18,14 +16,18 @@ from django.core.exceptions import PermissionDenied
 def survey(request):
     pk = request.user.pk
     user = User.objects.get(pk=pk)
+    form = UserForm(instance=user)
+    
     user.save()
 
-    
+
+
+
+
 
 
 
     return render(request, 'survey.html')
-    # form = UserForm(instance=user)
 
     # ProfileInlineFormset = inlineformset_factory(User, surveyForm, fields=('country',
     #                                                                    'city',
