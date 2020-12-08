816731abe03ce4a8c007825e3450db4f1167c3c4
trnguyen1510 <trnguyen1510@gmail.com>
2020-12-08 03:06:23 -0800

    heroku

--- voyagers/voyagers/settings.py
+++ voyagers/voyagers/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 import os
 
@@ -140,3 +141,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 print("STATIC URL", STATIC_URL)
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
